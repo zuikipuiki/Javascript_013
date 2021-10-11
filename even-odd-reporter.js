33f625a5f6d05446e61f7575f78b9b91f898b2ce
zuikipuiki <31111953+zuikipuiki@users.noreply.github.com>
2021-10-11 08:28:56 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,15 @@
+// LOOPS
+
+for (let i = 0; i <= 20; i++) {
+// console.log("Is " + i +  " even or odd?");
+
+ let remainder = (i % 2)
+ if (remainder == 0) {
+   console.log(i +  " is even")
+ }else{
+   console.log(i +  " is odd")
+ }
+   
+}
+
+
---
+++ multiplication-tables.js
@@ -0,0 +1,27 @@
+// LOOPS
+/*let multiply = "";
+for (let i = 1; i < 11; i++) {
+ for (let y = 1; y < 11; y++) {
+  multiply += (i*y) + " ";
+ }
+}
+console.log(multiply);*/
+
+for (i = 1; i <=9; i++) {
+ console.log(i * 9);
+}
+
+for (i = 1; i <=10; i++) {
+ console.log(i * 1);
+ console.log(i * 2);
+ console.log(i * 3);
+ console.log(i * 4);
+ console.log(i * 5);
+ console.log(i * 6);
+ console.log(i * 7);
+ console.log(i * 8);
+ console.log(i * 9);
+ console.log(i * 10);
+
+
+}
